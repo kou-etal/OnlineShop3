1c4bcf67b04694ca02b1c4a3e7dabd9209558274
etal <etal>
2025-06-04 22:42:00 +0900

    register VerifyCsrfToken

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,44 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+
+return Application::configure(basePath: dirname(__DIR__))
+
+    ->withBindings(function ($app) {
+        $app->singleton(
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\VerifyCsrfToken::class
+        );
+    })
+
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        channels: __DIR__.'/../routes/channels.php',
+        health: '/up',
+    )
+
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+
+        $middleware->alias([
+            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+        ]);
+
+        $middleware->append(HandleCors::class);
+    })
+
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
+
+/*
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -26,4 +61,4 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->create();*/
